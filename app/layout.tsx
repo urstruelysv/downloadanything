92b6245146e2249548cbe95347eddde89ba00ea5
Sai vamshi Gannoju <saivamshig404@gmail.com>
2025-06-09 18:31:14 +0530

    updated meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,25 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
-}
+  title: "Download anything",
+  description:
+    "DownloadAnything by Saivamshi Gannoju | AVL - Aethos Vision Labs. Effortlessly download videos and reels from YouTube, Facebook, Instagram, and more with a sleek, minimalistic UI.",
+  icons: {
+    icon: "/anything.png",
+    shortcut: "/anything.png",
+    apple: "/anything.png",
+  },
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
