49c97f7c0926e61efb9dd79a90a70c7ae607ff2c
Sai vamshi Gannoju <saivamshig404@gmail.com>
2025-06-09 19:31:48 +0530

    major fixes

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+  env: {
+    YOUTUBE_API_KEY: process.env.YOUTUBE_API_KEY,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
